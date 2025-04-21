7b302896dead2fef904696b12627d6af21f849bd
Pavel Demin <paveldemin@me.com>
2025-04-21 20:16:03 +0100

    feat: scaffold vite app

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [title, setTitle] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:8000").then(res => res.json()).then(txt => setTitle(txt))
+  })
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{title}</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
